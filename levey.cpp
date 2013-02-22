73de98449db5a8e2ebdc16165d88920e1d8d3902
tzp1021 <tzp1021@gmail.com>
2013-02-22 17:30:03 +0800

    Update levey.cpp

--- levey.cpp
+++ levey.cpp
@@ -1,1 +1,9 @@
  
+class Solution {
+public:
+    int numDistinct(string S, string T) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+    }
+};
