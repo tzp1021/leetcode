f78ab2c80994cefe1e8584ea9ca542c315b9ebe6
tzp1021 <tzp1021@gmail.com>
2018-09-27 16:47:51 +0800

    Update levey.cpp
    4. Median of Two Sorted Arrays

--- levey.cpp
+++ levey.cpp
@@ -183,4 +183,34 @@ public:
         return s.substr((ans_pos - 1 - ans_radius) / 2, ans_radius);
     }
 };
+//===========================Median of Two Sorted Arrays============================//
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int len1 = (int) nums1.size();
+        int len2 = (int) nums2.size();
+        if(len1 < len2)
+            return findMedianSortedArrays(nums2, nums1);
+        
+        int lo = 0, hi = len2 * 2;
+        while(lo <= hi) {
+            int mid2 = (lo + hi) / 2;
+            int mid1 = len1 + len2 - mid2;
+            
+            int l1 = (mid1 == 0) ? INT_MIN : nums1[(mid1 - 1) / 2];
+            int l2 = (mid2 == 0) ? INT_MIN : nums2[(mid2 - 1) / 2];
+            int r1 = (mid1 == len1 * 2) ? INT_MAX : nums1[mid1 / 2];
+            int r2 = (mid2 == len2 * 2) ? INT_MAX : nums2[mid2 / 2];
+            
+            if(l1 > r2) {
+                lo = mid2 + 1;
+            } else if(l2 > r1) {
+                hi = mid2 - 1;
+            } else {
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            }
+        }
+        return 0;
+    }
+};
 //===========================Distinct Subsequences============================//
