a3e0beca82bbf876e6ed039fb38dde267155421f
tzp1021 <tzp1021@gmail.com>
2018-09-28 16:25:57 +0800

    79. Word Search

--- levey.cpp
+++ levey.cpp
@@ -247,4 +247,31 @@ public:
         return 0;
     }
 };
-//===========================Distinct Subsequences============================//
+//===========================79. Word Search============================//
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i = 0; i < board.size(); i++) {
+            for(int j = 0; j < board[0].size(); j++) {
+                if(dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int idx) {
+        if(idx == word.length())
+            return true;
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[idx])
+            return false;
+        board[i][j] = '*';
+        bool ret = dfs(board, word, i, j + 1, idx + 1)
+                    || dfs(board, word, i, j - 1, idx + 1)
+                    || dfs(board, word, i - 1, j, idx + 1)
+                    || dfs(board, word, i + 1, j, idx + 1);
+        board[i][j] = word[idx];
+        return ret;
+    }
+};
+//===========================79. Word Search============================//
